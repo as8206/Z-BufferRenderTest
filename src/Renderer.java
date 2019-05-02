e9f65e28ae34b325d8a141f8fde9134274e13374
Andrew <andrewms98@live.com>
2019-05-02 01:31:52 -0400

    Polygons can now move, and there's more of them

--- src/Renderer.java
+++ src/Renderer.java
@@ -1,7 +1,5 @@
 import javax.swing.SwingUtilities;
 
-import sun.security.provider.certpath.BuildStep;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.BorderFactory;
@@ -30,14 +28,23 @@ public class Renderer
 	
 	public static ArrayList<Polygon> polygons = new ArrayList<Polygon>();
 	private static BufferedImage bufferedImage;
+	private static String back = "back";
+	private static String forward = "forward";
+	private static JFrame f;
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException
 	{
 		
 		bufferedImage = new BufferedImage();
-		polygons.add(new Polygon(buildShape(100, 100, 1, 100, 100, Color.green)));
-		polygons.add(new Polygon(buildShape(500, 450, 1, 321, 120, Color.red)));
-		polygons.add(new Polygon(buildShape(650, 30, 2, 45, 560, Color.blue)));
+		polygons.add(buildShape(100, 100, 0, 250, 250, Color.green, forward));
+		polygons.add(buildShape(500, 450, 15, 321, 120, Color.red, back));
+		polygons.add(buildShape(650, 30, 0, 45, 560, Color.blue, forward));
+		polygons.add(buildShape(162, 162, 15, 125, 125, Color.lightGray, back));
+		polygons.add(buildShape(300, 300, 12, 300, 250, Color.magenta, back));
+		polygons.add(buildShape(550, 200, 20, 78, 725, Color.darkGray, back));
+		polygons.add(buildShape(25, 760, 4, 333, 200, Color.pink, back));
+		polygons.add(buildShape(275, 830, 18, 600, 70, Color.cyan, forward));
+		polygons.add(buildShape(690, 690, 2, 250, 250, Color.orange, forward));
 		zBuffer(polygons);
 		
 		SwingUtilities.invokeLater(new Runnable() {
@@ -49,10 +56,22 @@ public class Renderer
 					e.printStackTrace();
 				} 
             }
-        }); 
+        });
+		
+		while(true)
+		{
+			Thread.sleep(200);
+			Update();
+			zBuffer(polygons);	
+			if(f != null)
+			{
+				f.revalidate();
+				f.repaint();
+			}
+		}
 	}
 	
-	private void Update()
+	private static void Update()
 	{
 		for(Polygon poly : polygons)
 		{
@@ -63,14 +82,34 @@ public class Renderer
 	private static void createAndShow() throws InterruptedException 
 	{
         System.out.println("Created on EDT? "+ SwingUtilities.isEventDispatchThread());
-        JFrame f = new JFrame("Z-Buffer Render Demo");
+        f = new JFrame("Z-Buffer Render Demo");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Component c = f.add(bufferedImage);
         f.pack();
         f.setVisible(true);
     }
 	
-	public static Pixel[] buildShape(int x, int y, int z, int length, int height, Color color)
+	public static Polygon buildShape(int x, int y, int z, int length, int height, Color color, String direction)
+    {
+    	Pixel rect[] = new Pixel[(length*height) + 1];
+    	rect[0] = new Pixel((length*height) + 1, (length*height) + 1, (length*height) + 1, Color.black);
+    	int index = 1;
+    	
+    	for(int i = x; i < x + length; i++)
+    	{
+    		for(int j = y; j < y + height; j++)
+    		{
+    			rect[index] = new Pixel(i, j, z, color);
+    			index++;
+    		}
+    	}
+    	
+    	Polygon poly = new Polygon(rect);
+    	poly.setDirection(direction);
+    	return poly;
+    }
+	
+	public static Pixel[] buildRender(int x, int y, int z, int length, int height, Color color)
     {
     	Pixel rect[] = new Pixel[(length*height) + 1];
     	rect[0] = new Pixel((length*height) + 1, (length*height) + 1, (length*height) + 1, Color.black);
@@ -90,12 +129,11 @@ public class Renderer
 	
 	public static void zBuffer(ArrayList<Polygon> polygons)
 	{
+		bufferedImage.reset();
 		for(Polygon poly : polygons)
 		{
 			for(int i = 1; i < poly.def[0].x; i++)
 			{
-//				System.out.println("Poly Z: "  +poly.def[i].z+ "Buffer z: " + bufferedImage.def[((poly.def[i].x-1) *1000) + poly.def[i].y].z );
-//				System.out.println("Poly index: " +i + "Buffer Index: " + (((poly.def[i].x-1) *1000) + poly.def[i].y));
 				if(poly.def[i].z > bufferedImage.def[((poly.def[i].x-1) *1000) + poly.def[i].y].z)
 				{
 					bufferedImage.def[((poly.def[i].x-1) *1000) + poly.def[i].y].z = poly.def[i].z;
@@ -110,6 +148,7 @@ public class Renderer
 class Polygon
 {
 	Renderer.Pixel def[];
+	private String direction;
 	
 	public Polygon(Renderer.Pixel[] def)
 	{
@@ -118,7 +157,38 @@ class Polygon
 	
 	public void update()
 	{
-		//TODO add movement here
+		if(def[1].z >= 20)
+			direction = "back";
+		else if (def[1].z <= 0)
+			direction = "forward";
+		move();
+	}
+	
+	public void setDirection(String dir)
+	{
+		direction = dir;
+	}
+	
+	private void move()
+	{
+		if (direction.equalsIgnoreCase("forward"))
+		{
+			for(int i = 1; i < def.length; i++)
+			{
+				def[i].z++;
+				def[i].x++;
+				def[i].y++;
+			}
+		}
+		else if (direction.equalsIgnoreCase("back"))
+		{
+			for(int i = 1; i < def.length; i++)
+			{
+				def[i].z--;
+				def[i].x--;
+				def[i].y--;
+			}
+		}
 	}
 }
 
@@ -128,7 +198,7 @@ class BufferedImage extends JPanel
 	
 	public BufferedImage()
 	{
-		Renderer.Pixel temp[] = Renderer.buildShape(0, 0, -1, 1000, 1000, Color.white);
+		Renderer.Pixel temp[] = Renderer.buildRender(0, 0, -1, 1000, 1000, Color.white);
 		this.def = temp;
 	}
 	
@@ -145,4 +215,9 @@ class BufferedImage extends JPanel
         	g.fillRect(def[i].x, def[i].y, 1, 1);
         }
     } 
+    
+    public void reset()
+    {
+    	def = Renderer.buildRender(0, 0, -1, 1000, 1000, Color.white);
+    }
 }
